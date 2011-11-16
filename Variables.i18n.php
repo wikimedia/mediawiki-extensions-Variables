8744cfc1b7d88d682bfbdcb2a574b9a4b8fcb973
Raimond Spekking <raymond@users.mediawiki.org>
2011-11-16 20:05:35 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Variables.i18n.php
+++ Variables.i18n.php
@@ -18,10 +18,31 @@ $messages['en'] = array(
 	'variables-desc' => 'Parser functions allowing to work with dynamic variables in an article scoped context',
 );
 
-/** German
+/** German (Deutsch)
  * @author Daniel Werner
  */
 $messages['de'] = array(
 	'variables-desc' => 'Bietet Parser-Funktionen zur Verwendung dynamischer Variablen in Wiki-Artikeln',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'variables-desc' => 'Funcións analíticas destinadas aos artigos que permiten traballar con variables dinámicas',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'variables-desc' => 'Парсерски функции што овозможуваат работа со динамични променливи во контекст со опфатност определена во страница',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'variables-desc' => 'Fungsi-fungsi penghurai yang membenarkan kerja dengan pembolehubah dinamk dalam konteks berskop rencana',
 );
 
