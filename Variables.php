b59f333d90e0f9a8aa6ecd909e273e4a77d44841
MGChecker <hgasuser@gmail.com>
2018-10-15 00:59:28 +0200

    Warn when using PHP entry point
    The PHP entry point is deprecated and will be removed soon.
    
    Change-Id: I56d2dc686131befbe734639a7914e09937e1dc15

--- Variables.php
+++ Variables.php
@@ -4,6 +4,10 @@ if ( version_compare( $wgVersion, '1.29', '>=' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Variables'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['VariablesMagic'] = __DIR__ . '/Variables.i18n.magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for Cite extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return;
 } else {
 	die( 'This version of the Variables extension requires MediaWiki 1.29+' );
