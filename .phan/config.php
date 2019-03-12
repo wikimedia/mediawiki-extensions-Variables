db3e18fdd23407d7a1377e7c595de8b07b8028d3
MGChecker <hgasuser@gmail.com>
2019-03-12 02:30:59 +0100

    Migrate to newer phan
    Unfortunately, this adds to the clutter in the extension
    root file.
    
    Bug: T216950
    Change-Id: I231ed036c7d6f320b5cbe89a2f52ee2915af4c60

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$config = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 // Due to creation of Parser::$mExtVariables property
 $config['suppress_issue_types'][] = 'PhanUndeclaredProperty';
 
